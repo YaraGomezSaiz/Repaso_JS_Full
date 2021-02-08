cb979f378cc929c4a04e7b31be0c86e6be7d822c
YARA-G <y.gomez.saiz@hotmail.com>
2021-02-08 20:00:20 +0000

    Ej.10

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let names = ["alvaro ramos", "fede gomez", "luna gomez", "Yara gomez",];
-let ages = [31, 60, 35, 32];
+let names = ["alvaro ramos", "fede gomez", "luna gomez", "Yara gomez", "Vera Martinez"];
+let ages = [31, 60, 35, 32, 1];
 let obj = { name: "Maite", surname: "Saiz", age: 60 };
 let People = [];
 let People_less18 = [];
@@ -10,19 +10,26 @@ let Parents = [];
 //funcion que se ejecuta al cargar la pagina, genera la lista de nombres ya almacenados en el array, 
 //y crea el evento "click" para que cada vez que pulse el boton "submit de HTML" inserte el nuevo elemento.
 window.onload = function () {
+    //Generar un evento click cada vez que se pulse boton "save" para guardar una persona nueva
     const InputButtonSubmit = document.getElementById("InputButton");
     InputButtonSubmit.addEventListener("click", f_GetHTMLInput)
     f_ShowList(names, ages);
+
 }
 
 //Funcion que obtiene el nombre y edad introducido en HTML y llama a la funcion que lo introduce en la lita
 
 function f_GetHTMLInput() {
+
     const InputName = document.querySelector("#InputName");
-    names.push(InputName.value);
-    const InputAge = document.getElementById("InputAge");
-    ages.push(InputAge.value);
-    f_PeopleListGenerator(InputName.value, InputAge.value);
+    if (InputName.value!= "" && InputAge.value>0) {
+        names.push(InputName.value);
+        const InputAge = document.getElementById("InputAge");
+        ages.push(InputAge.value);
+        f_PeopleListGenerator(InputName.value, InputAge.value);
+    }
+    console.log(InputName.value);
+        console.log(InputAge.value);
 }
 
 
@@ -112,8 +119,12 @@ function f_DetectParent() {
 //Funcion que inserta una lista los objetos dentro de una lista en HTML
 function SetInHTML(name, surname, age, id) {
     let NameList = document.createElement("tr");
+    //<td id= "${name} ${surname}" type="button" data-toggle="modal" data-target="#exampleModal" data-whatever="${name} ${surname} : ${age} years">
     NameList.innerHTML = `
-            <td> ${name} </td>
+   <td id= "${name}" type="button" data-toggle="modal" data-target="#exampleModal" data-whatever="${name} ${surname} : ${age} years">
+   
+        ${name} 
+    </td>    
             <td> ${surname}   </td>
             <td> ${age}   </td>`
     let HTMLList = document.querySelector(id);
@@ -121,6 +132,39 @@ function SetInHTML(name, surname, age, id) {
 }
 
 
+//funcion que recibe los datos desde el boton donde se lanza el modal y modifica su contenido
+
+$('#exampleModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text(recipient)
+
+    //boton Delete
+    const ModalButtonDelete = document.getElementById("ModalDeleteButton");
+    ModalButtonDelete.addEventListener("click", () => {
+
+        let Fullname = f_fistletterToUpperCase(recipient);
+        let name = Fullname[0];
+        let surname = Fullname[1];
+
+        for (let i = 0; i < People.length; i++) {
+            if (People[i].name == name && People[i].surname == surname) {
+                People.splice(i, 1);
+                id = "#" + name;
+                //id = "#"+name+" "+surname;
+                console.log(id);
+                let getchild = document.querySelectorAll(id);
+                getchild[0].parentElement.remove();
+                getchild[1].parentElement.remove();
+            }
+        }
+    })
+})
+
+/*
 //Dado un objeto introducirlo en la lista HTML
 function f_objectToHTML(p_obj) {
     let name = p_obj.name;
@@ -128,5 +172,4 @@ function f_objectToHTML(p_obj) {
     let age = p_obj.age;
     SetInHTML(name, surname, age, "#mainList");
 }
-(f_objectToHTML(obj));
-
+(f_objectToHTML(obj));*/
